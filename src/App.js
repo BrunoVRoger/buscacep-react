42636607367e9c873568fe24d461d3e74b4029e2
Bruno <bruno.vinicius@bcrservicos.com.br>
2020-12-30 11:51:29 -0300

    adiçao do footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './GlobalStyle.css'
 
 /* import Header from './components/Header/index'; */
 import Form from './components/Form/index';
+import Footer from './components/Footer/index';
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Footer/>
 
     </div>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import footer from '../../assets/logo_footer.png';
+import './style.css'
+ 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="img-footer">
+            <img  src={footer} ></img>
         </div>
     )
 }
-
 export default Footer;
